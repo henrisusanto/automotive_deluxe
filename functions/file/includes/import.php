12dc98fb0d3d2db4901b701f399dc94b2bd7d0a1
indosystem <indosystem@Edys-MacBook-Pro.local>
2015-11-18 04:47:52 +0700

    last changes from Phill

--- functions/file/includes/import.php
+++ functions/file/includes/import.php
@@ -9,30 +9,30 @@ $conn_id = ftp_connect('ftp1.trader.com');
 ftp_login($conn_id, 'ONCE_FusionAS', ')15ueODb[n');
 $remote_files = ftp_nlist($conn_id, '/');
 $downloaded = array();
-		
+
 if(isset($post['file-type']) && isset($post['file-name'])){
 	echo '<div id="progressbar"><div class="progress-label">0%</div></div>';
 	echo '<script type="text/javascript">updateProgress(0,\'0%\')</script>';
 	ob_flush();
 	flush();
 	sleep(1);
-	
+
 	//get the file to loop through the records
 	$post['file-name'] = $_POST['file-name'];
 	$importFile = $post['file-name'];
-	
+
 	if($post['file-type']=='xml'){
-		
+
 		$fileData = file_get_contents(GTCDI_DIR . '/' . $importFile);
-		
+
 		$dom = new DomDocument();
 		$dom->loadXML($fileData);
 		$xmlArray = gtcd_xml_to_array($dom);
-		
+
 		$xmlRecords = gtcd_xml_records($xmlArray, $post['file-path']);
 		$importFields = gtcd_get_xpath($xmlArray,$post['file-path']);
 		$importFields = gtcd_get_keys($importFields);
-		
+
 		$post_types = array();
 		foreach($xmlRecords as $key=>$value){
 			$post_types[$key]['title'] = $value[$post['mapTitle']];
@@ -49,7 +49,7 @@ if(isset($post['file-type']) && isset($post['file-name'])){
 			}
 			fclose($handle);
 		}
-		
+
 		$columns = array_shift($importData);
 
 		$importRecords = array();
@@ -57,19 +57,20 @@ if(isset($post['file-type']) && isset($post['file-name'])){
 			foreach($value as $key1=>$value1){
 				$importRecords[$key][$columns[$key1]] = $value1;
 			}
-		}		
-		
-		// trim attributes name 
+		}
+
+		// trim attributes name
 		foreach ($importRecords[$line] as $tr => $im) {
 			unset($importRecords[$line][$tr]);
 			$importRecords[$line][trim($tr)] = $im;
 		}
-			
+
 		// ftp download csv.photos into vehicle.photos
 		$postPhotos = array();
 		foreach ($importRecords[$line] as $va => $lue) {
 			if (!strpos($lue, '.jpg')) continue;
 			$img = explode(',', $lue);
+
 			foreach ($img as $i => $mg) {
 				$download = trim($mg);
 				if (strlen($download) < 1) continue;
@@ -77,24 +78,32 @@ if(isset($post['file-type']) && isset($post['file-name'])){
 				if (file_exists(get_home_path() . $download)) continue;
 				if (ftp_get($conn_id, get_home_path() . $download, $download, FTP_BINARY)) {
 						$downloaded[] = get_home_path() . $download;
-						$postPhotos[] = site_url($download); 
+						$postPhotos[] = site_url($download);
 					}
 				}
 				$importRecords[$line][$va] = '';
 			}
+
 			ftp_close($conn_id);
-			
+
 			// mapping csv.options as vehicle.features
 			$post['mapTax']['features'] = Array("field" => "Options","separator" => ",");
 			$post_type['title'] = $importRecords[$line]['Make'] . ' ' . $importRecords[$line]['Model'];
 			$post_type['meta'] = gtcd_map_meta_fields($importRecords[$line],$post['mapMeta']);
 			$post_type['tax'] = gtcd_map_tax_fields($importRecords[$line],$post['mapTax']);
 			$post_type['photos'] = $postPhotos;
+
+			// make & model
+			$post_type['_make'] = !empty($importRecords[$line]['Make']) ? $importRecords[$line]['Make'] : '';
+			$post_type['_model'] = !empty($importRecords[$line]['Model']) ? $importRecords[$line]['Model'] : '';
+			// category
+			$post_type['_category'] = !empty($importRecords[$line]['Category']) ? $importRecords[$line]['Category'] : '';
+			// end.
 	}
 
 	$listing_totals = gtcdi_import_records(array($post_type));
 	foreach ($downloaded as $dl) @unlink($dl);
-	if ($line == $import_limit) { 
+	if ($line == $import_limit) {
 		@unlink(GTCDI_DIR . '/' . $importFile);
 		echo 'ALL DONE';
 	} else {
@@ -107,11 +116,11 @@ if(isset($post['file-type']) && isset($post['file-name'])){
 			<input type="hidden" name="csvline" value="<?= $line ?>" />
 		</form>
 		<script type="text/javascript">
-			if (jQuery('b:contains("Warning")').length < 1) 
+			if (jQuery('b:contains("Warning")').length < 1)
 				jQuery('form#bridge_form').submit()
 			else jQuery('form#bridge_form')
-				.append('Import process stopped because incorrect information on vehicle in line ' + 
-				jQuery('input[name="csvline"]').val()) 
+				.append('Import process stopped because incorrect information on vehicle in line ' +
+				jQuery('input[name="csvline"]').val())
 		</script>
 		<?php
 	}
